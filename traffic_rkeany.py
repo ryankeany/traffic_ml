2fb101d03d8993630901426523cbeaafc308a8ec
”Ryan <rkeany@calpoly.edu>
2024-11-17 18:20:16 -0800

    fix bug

--- traffic_rkeany.py
+++ traffic_rkeany.py
@@ -129,9 +129,9 @@ if file:
     df["Upper Prediction Limit"] = np.round(intervals[:, 1], 2)
 
     # Add results to the original DataFrame
-    df1["Predicted Volume"] = df["Predicted Volume"].round(1)
-    df1["Lower Prediction Limit"] = df["Lower Prediction Limit"].round(1)
-    df1["Upper Prediction Limit"] = df["Upper Prediction Limit"].round(1)
+    df1["Predicted Volume"] = round(df["Predicted Volume"], 1)
+    df1["Lower Prediction Limit"] = round(df["Lower Prediction Limit"], 1)
+    df1["Upper Prediction Limit"] = round(df["Upper Prediction Limit"], 1)
 
     # Display the dataframes
     st.dataframe(data=df1)
